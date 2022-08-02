e598db2181a6cad94e0a6550b64da311b59cfee9
Furkan Yılmaz <frknylmz1566@gmail.com>
2022-08-03 00:24:13 +0300

    gameBoard eklendi.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
+        int[][] gameBoard= new int[10][10];
+        for(int i=0;i< gameBoard.length ;i++){
+            System.out.println();
+            for (int j=0;j< gameBoard.length;j++){
+                System.out.print(gameBoard[i][j]+"\t");
+            }
+        }
+
+
     }
 }
